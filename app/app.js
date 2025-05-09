5a06dd5df846cda8c7762aa8abcd38c9f4f20529
Dev Faizan <faixanmir772@gmail.com>
2025-05-09 12:15:53 +0530

    chore(app): configure CORS to allow all origins with explicit headers and methods

--- app/app.js
+++ app/app.js
@@ -4,6 +4,7 @@ const connectDB = require("./config/db.config.js");
 const dotenv = require("dotenv");
 const professionalRoutes = require("../app/routes/professional.routes.js");
 const driverRoutes = require("../app/routes/driver.routes.js");
+const userRoutes = require("../app/routes/user.routes.js");
 const scheduleRoutes = require("../app/routes/schedule.routes.js");
 const errorMiddleware = require("./middlewares/error.middleware.js");
 const notFoundMiddleware = require("./middlewares/notFound.middleware.js");
@@ -12,16 +13,24 @@ dotenv.config();
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/professionals", professionalRoutes);
+app.use("/api/drivers", driverRoutes);
+app.use("/api/schedules", scheduleRoutes);
 
 // Error handler
 app.use(errorMiddleware);
 app.use(notFoundMiddleware);
-
-// Routes
-app.use("/api/professionals", professionalRoutes);
-app.use("/api/drivers", driverRoutes);
-app.use("/api/schedules", scheduleRoutes);
 
 module.exports = app;
