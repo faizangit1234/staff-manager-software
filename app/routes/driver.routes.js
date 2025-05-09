4e0b13fb4a815bc965c08ce28c1a779ba1da5ff1
Dev Faizan <faixanmir772@gmail.com>
2025-05-09 12:16:09 +0530

    refactor(routes): update driver routes structure and ensure proper middleware usage

--- app/routes/driver.routes.js
+++ app/routes/driver.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const validateToken = require("../middlewares/validateToken.middleware.js");
+const checkrole = require("../middlewares/checkRole.js");
 const {
   getDrivers,
   postDriver,
@@ -8,7 +10,14 @@ const {
   deleteDriver,
 } = require("../controllers/driver.controllers.js");
 
-router.route("/").get(getDrivers).post(postDriver);
-router.route("/:id").get(getDriverByID).put(updateDriver).delete(deleteDriver);
+router
+  .route("/")
+  .get(validateToken, checkrole("admin", "superAdmin"), getDrivers)
+  .post(validateToken, checkrole("admin", "superAdmin"), postDriver);
+router
+  .route("/:id")
+  .get(validateToken, checkrole("admin", "superAdmin"), getDriverByID)
+  .put(validateToken, checkrole("admin", "superAdmin"), updateDriver)
+  .delete(validateToken, checkrole("admin", "superAdmin"), deleteDriver);
 
 module.exports = router;
