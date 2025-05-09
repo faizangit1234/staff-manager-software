af8ae0ee89eda81677dfaa8268f7886468fffb1e
Dev Faizan <faixanmir772@gmail.com>
2025-05-09 12:16:15 +0530

    refactor(routes): clean up and enhance professional routes for clarity and consistency

--- app/routes/professional.routes.js
+++ app/routes/professional.routes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const validateToken = require("../middlewares/validateToken.middleware.js");
+const checkrole = require("../middlewares/checkRole.js");
 const {
   getProfessionals,
   postProfessional,
@@ -8,13 +10,16 @@ const {
   deleteProfessional,
 } = require("../controllers/professional.controllers.js");
 
-router.route("/").get(getProfessionals).post(postProfessional);
+router
+  .route("/")
+  .get(validateToken, checkrole("admin", "superAdmin"), getProfessionals)
+  .post(validateToken, checkrole("admin", "superAdmin"), postProfessional);
 
 // GET - Get professional by ID
 router
   .route("/:id")
-  .get(getProfessionalByID)
-  .put(updateProfessional)
-  .delete(deleteProfessional);
+  .get(validateToken, checkrole("admin", "superAdmin"), getProfessionalByID)
+  .put(validateToken, checkrole("admin", "superAdmin"), updateProfessional)
+  .delete(validateToken, checkrole("admin", "superAdmin"), deleteProfessional);
 
 module.exports = router;
